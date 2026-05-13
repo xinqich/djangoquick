50c2eeb663af4823191a1daccb1a3eaca32d0938
xinqich <timurcallo@gmail.com>
2026-05-13 17:16:14 +0300

    implement authentication

--- app/authenticate/models.py
+++ app/authenticate/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password=password, **extra_fields)
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email adress',
+        max_length=255,
+        unique=True
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
